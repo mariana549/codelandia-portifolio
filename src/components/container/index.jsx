3eef9f7d88370d7f3a28beb8aea817d0e0c6f004
Mariana Antonia <marievaristo889@gmail.com>
2024-05-06 11:01:53 -0300

    refactor: :recycle: refatorando
    mariana antonia

--- src/components/container/index.jsx
+++ src/components/container/index.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 function Container({children}) {
    return (
-      <div className="max-w-6xl mx-auto p-4 ">
+      <div className="max-w-6xl mx-auto p-4 max-lg:p-6 ">
          {children}
       </div>
    )
