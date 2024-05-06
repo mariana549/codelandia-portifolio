8cb7e10369b7e26a7c118edd9f89ed917861d67a
Mariana Antonia <marievaristo889@gmail.com>
2024-05-06 11:09:30 -0300

    refactor: :recycle: refatorando
    mariana antonia

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -5,10 +5,10 @@ function Footer() {
    return (
       <Container>
          <footer className="flex items-center justify-between  w-full pt-12 pb-4">
-            <img src={logoPequena} alt="logo" className="size-10"/>
+            <img src={logoPequena} alt="logo" className="size-10 max-lg:size-9 max-md:size-8 max-sm:size-6"/>
             <div className="flex gap-7 items-center">
-               <i className="text-[#AEAEAE] text-lg" >© 2024 - Feito por Mariana Antonia </i>
-               <span className="text-[#AEAEAE] text-lg">Projeto Codelândia</span>
+               <i className="text-[#AEAEAE] text-lg max-lg:text-base max-md:text-sm max-sm:text-xs" >© 2024 - Feito por Mariana Antonia </i>
+               <span className="text-[#AEAEAE] text-lg max-lg:text-base max-md:text-sm max-sm:text-xs">Projeto Codelândia</span>
             </div>
          </footer>
       </Container>
