5504c4e535f1860fa72940f7418eabec1d886cdb
Mariana Antonia <marievaristo889@gmail.com>
2024-05-06 10:57:55 -0300

    refactor: :recycle: refatorando
    mariana antonia

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,15 +6,18 @@ function Header({ github, getUser }) {
    return (
       <header className="py-4">
          <Container>
-            <div className="flex items-center justify-between">
-               <img src={logo} alt="logo"  />
+            <div className="flex items-center justify-between max-lg:flex-col">
+               <img src={logo} alt="logo" className="w-60 max-lg:w-52 max-md:w-48 max-sm:w-44 max-lg:mb-5" />
                <nav>
-                  <ul className="flex items-center gap-6">
+                  <ul className="flex items-center gap-6 
+                  max-md:flex-col">
                      <li className="flex gap-2">
                         <input
                            id="user"
                            type="text"
-                           className="text-white bg-black text-xl border-2 border-[#873EFF] hover:shadow-md hover:shadow-violet-600 rounded px-4 py-1"
+                           className="text-white bg-black text-xl border-2 border-[#873EFF] rounded px-4 py-1
+                           w-60
+                           hover:shadow-md hover:shadow-violet-600 "
                            onChange={(e) => getUser(e.target.value)}
                            placeholder="Digite seu Usuário"
                         />
@@ -26,7 +29,7 @@ function Header({ github, getUser }) {
                         </button>
                      </li>
                      <li>
-                        <a className="text-white text-xl border-2 border-[#873EFF] rounded px-4 py-1 hover:bg-violet-600" href={github}>Github</a>
+                        <a className="text-white text-xl border-2 border-[#873EFF] rounded px-4 py-1 hover:bg-violet-600 max-md:hidden" href={github} >Github</a>
                      </li>
                   </ul>
                </nav>
